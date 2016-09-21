ea590933cf3ba8bec071f51675ed0577aff693cc
Qubo <g2jedhq@qq.com>
2016-09-21 14:35:52 +0800

    First commit.

---
+++ app/src/main/java/com/qubo/goodweather/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qubo.goodweather.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.qubo.goodweather.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
